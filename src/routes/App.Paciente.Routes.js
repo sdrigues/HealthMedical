57fb2dc0a33d7534f784ae7246628eab80f0a6ed
sdrigues <rodrigues.sam97@gmail.com>
2020-11-20 13:36:00 -0300

    alteração de repositório para deploy

--- src/routes/App.Paciente.Routes.js
+++ src/routes/App.Paciente.Routes.js
@@ -5,7 +5,7 @@ import Dashboard_Paciente from '../Pages/paciente/Dashboard_Paciente';
 import Agendas_Paciente from '../Pages/paciente/Agenda';
 import Rede_Credenciada from '../Pages/paciente/Rede_Credenciada';
 import Agendar_Consulta from '../Pages/paciente/Agendar_Consulta';
-// import Agendamento_Consulta from '../Pages/paciente/Agendamento_Consulta';
+import Agendamento_Consulta from '../Pages/paciente/Agendamento_Consulta';
 
 
 export default function Routes(){
@@ -16,7 +16,7 @@ export default function Routes(){
                 <Route path="/agendas_Paciente" exact component={Agendas_Paciente} />
                 <Route path="/rede_Credenciada" exact component={Rede_Credenciada} />
                 <Route path="/agendar_Consulta/:id" exact component={Agendar_Consulta} />
-                {/*<Route path="/agendamento_Consulta" exact component={Agendamento_Consulta} /> */}
+                <Route path="/agendamento_Consulta" exact component={Agendamento_Consulta} />
             </Switch>
         </BrowserRouter>
     );

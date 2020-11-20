5d6ef4e14c73a22abfd21cd58d7b381ba4ba3715
sdrigues <rodrigues.sam97@gmail.com>
2020-11-20 14:00:02 -0300

    alteração de repositório para deploy

--- src/routes/App.Secretaria.Routes.js
+++ src/routes/App.Secretaria.Routes.js
@@ -1,12 +1,11 @@
 import React from 'react'
-// import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import { Switch, BrowserRouter } from 'react-router-dom'
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 
-// import Dashboard_Secretaria from '../pages/secretaria/Dashboard_Secretaria'
-// import Agenda_Medica from '../pages/secretaria/Agenda';
-// import Rede_Credenciada from '../pages/secretaria/Rede_Credenciada';
-// import Agenda_medico from '../pages/secretaria/Agenda_medico'
-// import Agendar_Consulta from '../pages/secretaria/Agendar_Consulta';
+import Dashboard_Secretaria from '../Pages/secretaria/Dashboard_Secretaria'
+// import Agenda_Medica from '../Pages/secretaria/Agenda';
+// import Rede_Credenciada from '../Pages/secretaria/Rede_Credenciada';
+// import Agenda_medico from '../Pages/secretaria/Agenda_medico'
+// import Agendar_Consulta from '../Pages/secretaria/Agendar_Consulta';
 
 
 
@@ -14,8 +13,8 @@ export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                {/* <Route path="/dashboard_secretaria" exact component={Dashboard_Secretaria} />
-                <Route path="/agenda_medica" exact component={Agenda_Medica} />
+                <Route path="/dashboard_secretaria" exact component={Dashboard_Secretaria} />
+                {/* <Route path="/agenda_medica" exact component={Agenda_Medica} />
                 <Route path="/rede_Credenciada" exact component={Rede_Credenciada} />
                 <Route path="/agenda_medico/:id" exact component={Agenda_medico} /> 
                 <Route path="/agendar_Consulta" exact component={Agendar_Consulta} /> */}
--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,7 +5,7 @@ import AuthContext from '../contexts/Auth';
 //pages
 import Appauth from './App.Auth.Routes';
 import AppPaciente from './App.Paciente.Routes';
-// import AppSecretaria from './App.Secretaria.Routes';
+import AppSecretaria from './App.Secretaria.Routes';
 // import AppMedico from './App.Medico.Routes';
 
 
@@ -16,10 +16,10 @@ export default function Router() {
         history.push("/dashboard_Paciente");
         return <AppPaciente />
     }
-    // if(signed === true && tipo_usuario === 'secretaria' ){
-    //     history.push("/dashboard_secretaria");
-    //     return <AppSecretaria />
-    // } 
+    if(signed === true && tipo_usuario === 'secretaria' ){
+        history.push("/dashboard_secretaria");
+        return <AppSecretaria />
+    } 
     // if(signed === true && tipo_usuario === 'medico' ){
     //     history.push("/dashboard_Medico");
     //     return <AppMedico />

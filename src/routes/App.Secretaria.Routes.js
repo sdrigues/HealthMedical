9aa5dc53264026ad3296903d342bee6fb941accd
sdrigues <rodrigues.sam97@gmail.com>
2020-11-23 19:15:33 -0300

    alteração de repositório para deploy

--- src/routes/App.Secretaria.Routes.js
+++ src/routes/App.Secretaria.Routes.js
@@ -5,7 +5,7 @@ import Dashboard_Secretaria from '../Pages/secretaria/Dashboard_Secretaria'
 import Agenda_Medica from '../Pages/secretaria/Agenda';
 import Rede_Credenciada from '../Pages/secretaria/Rede_Credenciada';
 import Agenda_medico from '../Pages/secretaria/Agenda_medico'
-// import Agendar_Consulta from '../Pages/secretaria/Agendar_Consulta';
+import Agendar_Consulta from '../Pages/secretaria/Agendar_Consulta';
 
 
 
@@ -17,7 +17,7 @@ export default function Routes() {
                 <Route path="/agenda_medica" exact component={Agenda_Medica} />
                 <Route path="/rede_Credenciada" exact component={Rede_Credenciada} />
                 <Route path="/agenda_medico/:id" exact component={Agenda_medico} /> 
-                {/* <Route path="/agendar_Consulta" exact component={Agendar_Consulta} /> */}
+                <Route path="/agendar_Consulta" exact component={Agendar_Consulta} />
             </Switch>
         </BrowserRouter>
     );
